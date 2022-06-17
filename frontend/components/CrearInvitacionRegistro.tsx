21e8a6f9e7316dbf6f5fd72df71d0059f4beb86f
alfman99 <amanresamntz@gmail.com>
2022-06-17 13:28:51 +0200

    fix linting problem

--- frontend/components/CrearInvitacionRegistro.tsx
+++ frontend/components/CrearInvitacionRegistro.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Table } from "@mantine/core";
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../contexts/AuthContext";
